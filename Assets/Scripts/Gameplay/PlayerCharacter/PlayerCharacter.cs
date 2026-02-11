c99711b6dfe9177f92bbee9f223a7e5d8cf7cfe2
Serhiiko <serikgorbanev@gmail.com>
2026-02-12 00:26:52 +0100

    Made use of PlayerLoadout

--- Assets/Scripts/Gameplay/PlayerCharacter/PlayerCharacter.cs
+++ Assets/Scripts/Gameplay/PlayerCharacter/PlayerCharacter.cs
@@ -1,3 +1,5 @@
+using System;
+using Tools;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -7,14 +9,28 @@ namespace Gameplay.PlayerCharacter
     {
         [SerializeField] private GameObject _PlayerCharacterControllerPrefab;
     
+        public PlayerLoadout _PlayerLoadout;
+
+        private Action _onSpawned;
+        public event Action OnSpawned
+        {
+            add => _onSpawned += value;
+            remove => _onSpawned -= value;
+        }
+        
         public override void OnNetworkSpawn()
         {
+            NetworkObject playerState = this.GetPlayerObjectOfOwner();
+            _PlayerLoadout = playerState.GetComponent<PlayerLoadout>();
+            
             if (IsOwner)
             {
                 GameObject controller = Instantiate(_PlayerCharacterControllerPrefab, transform);
                 controller.GetComponent<PlayerCharacterController>();
                 OwnedPlayerSpawnEventBus.Invoke(this);
             }
+
+            _onSpawned?.Invoke();
         }
     
         public override void OnNetworkDespawn()
